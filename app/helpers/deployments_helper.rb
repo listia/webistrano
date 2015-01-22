64d4173e0ecc7bece86cbf804ecfc83b32989749
ilake <lake.ilakela+github@gmail.com>
2015-01-22 17:27:36 +0800

    Fix precheck functional

--- app/helpers/deployments_helper.rb
+++ app/helpers/deployments_helper.rb
@@ -9,23 +9,28 @@ module DeploymentsHelper
   end
 
   def if_disabled_host?(host, role, text)
-    if @deployment.excluded_host_ids.include?(host.id) || !role.precheck
-      text
-    else
+    if deployment_check_this_host?(host, role)
       ''
+    else
+      text
     end
   rescue
     ''
   end
 
   def if_enabled_host?(host, role, text)
-    if @deployment.excluded_host_ids.include?(host.id) || !role.precheck
-      ''
-    else
+    if deployment_check_this_host?(host, role)
       text
+    else
+      ''
     end
   rescue
     text
+  end
+
+  private
+  def deployment_check_this_host?(host, role)
+    !@deployment.excluded_host_ids.include?(host.id) && role.precheck
   end
 
 end
